d84470ece49b5b4a7eeffd86253430cddfd657db
Ashmhill <amh110@byui.edu>
2025-09-29 18:13:06 -0600

    Sep 29 @ 6:12 PM

--- week-2/app.js
+++ week-2/app.js
@@ -9,6 +9,11 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/", routes);
 
+// Add a friendly root route
+app.get("/", (req, res) => {
+  res.send("Welcome to the Contacts API! Use /contacts to get all contacts or /contacts/:id to get a single contact.");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
